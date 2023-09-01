5ae4dee31e25f5ec1306952b6b6bf56664cf6ed4
dsilvau <125933418+dsilvau@users.noreply.github.com>
2023-08-31 23:49:37 -0500

    22

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -27,13 +27,5 @@ class Grupo:
         
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
-
-    @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
         cls.grado = nombre
